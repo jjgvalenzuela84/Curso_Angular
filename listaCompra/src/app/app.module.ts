a571b7dd5c34cd253d55eb64d98717c920ea543a
jjgv <jjgvalenzuela@gmail.com>
2017-09-15 09:38:55 +0200

    .

--- listaCompra/src/app/app.module.ts
+++ listaCompra/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PrincipalModule } from './principal/principal.module';
 
- 
 @NgModule({
   declarations: [
     AppComponent
